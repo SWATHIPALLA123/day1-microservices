750a812758ca6bde7324e1f44243b446473fecea
Swathi <swathipalla870@gmail.com>
2026-08-20 14:55:26 +0530

    Add Day 3 API Gateway

---
+++ day3/day3-api-gateway/day3-api-gateway/src/main/java/com/example/day3gateway/Day3ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.day3gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day3ApiGatewayApplication.class, args);
+	}
+
+}
---
+++ day3/day3-api-gateway/day3-api-gateway/src/test/java/com/example/day3gateway/Day3ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day3gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day3ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
